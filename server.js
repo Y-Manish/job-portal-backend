b5d4ccd458e333192f79be8c44781f3878293c7f
Y-Manish <manish.000587@gmail.com>
2026-08-26 18:01:29 +0530

    Add temporary job CRUD endpoints

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express"
 
 const app = express()
 const PORT = 4000
+app.use(express.json())//middleware to convert that JSON into req.body
 
 app.get("/",(req,res)=>{
     res.send("Job Portal is running");
@@ -50,6 +51,84 @@ app.get("/api/jobs/:id",(req,res)=>{
     });
 });
 
+//Add new Job 
+app.post("/api/jobs",(req,res)=>{
+    const newJob = req.body
+      // Validate required fields
+  if (!newJob.title || !newJob.company || !newJob.location) {
+    return res.status(400).json({
+      success: false,
+      message: "Title, company and location are required",
+    });
+  }
+  //Generate new id
+  newJob.id =  jobs.length+1
+
+  //Add new job to the array
+  jobs.push(newJob)
+    // Send response
+  res.status(201).json({
+    success: true,
+    message: "Job created successfully",
+    data: newJob,
+  });
+})
+
+//Update job by id
+// Update job by id
+app.patch("/api/jobs/:id", (req, res) => {
+  // Read ID from URL
+  const jobId = Number(req.params.id);
+
+  // Find the job position
+  const jobIndex = jobs.findIndex((eachJob) => eachJob.id === jobId);
+
+  // Check whether the job exists
+  if (jobIndex === -1) {
+    return res.status(404).json({
+      success: false,
+      message: "Job not found",
+    });
+  }
+
+  // Update the existing job with the fields sent by the client
+  jobs[jobIndex] = {
+    ...jobs[jobIndex],
+    ...req.body,
+    id: jobId,
+  };
+
+  res.status(200).json({
+    success: true,
+    message: "Job updated successfully",
+    data: jobs[jobIndex],
+  });
+});
+
+app.delete("/api/jobs/:id", (req, res) => {
+  // Read ID from URL
+  const jobId = Number(req.params.id);
+
+  // Find the job position
+  const jobIndex = jobs.findIndex((eachJob) => eachJob.id === jobId);
+
+  // Check whether the job exists
+  if (jobIndex === -1) {
+    return res.status(404).json({
+      success: false,
+      message: "Job not found",
+    });
+  }
+
+  // Remove one job from the array
+  const deletedJob = jobs.splice(jobIndex, 1);
+
+  res.status(200).json({
+    success: true,
+    message: "Job deleted successfully",
+    data: deletedJob[0],
+  });
+});
 
 app.listen(PORT,()=>{
     console.log(`Server is running at the port ${PORT}`)
